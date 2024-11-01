ab1b6892dd5f6a15f40373d5aa70cf056ea506fa
lilonghe <lilonghe@outlook.com>
2024-11-01 18:15:58 +0800

    feat: camera detail modal animation

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -1,21 +1,48 @@
 'use client'
 
+import { lockBodyScroll, unlockBodyScroll } from "@/utils";
 import { useRouter } from "next/navigation";
 import { ElementRef, MouseEvent, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-export default function Modal({ children }: { children: React.ReactNode }) {
+export default function Modal({ children, className = '' }: { children: React.ReactNode, className?: string }) {
   const router = useRouter();
   const dialogRef = useRef<ElementRef<'dialog'>>(null);
+  const wrapperRef = useRef<ElementRef<'div'>>(null);
 
   useEffect(() => {
-    if (!dialogRef.current?.open) {
+    if (dialogRef.current && wrapperRef.current) {
+      dialogRef.current.style.opacity = '0';
+      dialogRef.current.style.transition = 'transform .3s, opacity .3s';
+      wrapperRef.current.style.opacity = '0';
+      wrapperRef.current.style.transition = 'transform .3s, opacity .3s';
+    }
+
+    if (dialogRef.current && !dialogRef.current?.open) {
       dialogRef.current?.showModal();
+      lockBodyScroll();
+
+      setTimeout(() => {
+        if (dialogRef.current && wrapperRef.current) {
+          dialogRef.current.style.opacity = '1';
+          wrapperRef.current.style.opacity = '1';
+        }
+        if (wrapperRef.current) {
+          wrapperRef.current.style.opacity = '1';
+        }
+      })
     }
   }, []);
 
   const handleDismiss = () => {
-    router.back();
+    if (dialogRef.current && wrapperRef.current) {
+      dialogRef.current.style.opacity = '0';
+      wrapperRef.current.style.opacity = '0';
+    }
+    setTimeout(() => {
+      unlockBodyScroll();
+      router.back();
+    }, 260)
   }
 
   const handleDialogClick = (e: MouseEvent<HTMLElement>) => {
@@ -23,7 +50,7 @@ export default function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-30">
+    <div ref={wrapperRef} className={"fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 " + className}>
       <dialog ref={dialogRef} onClick={handleDismiss} onClose={handleDismiss} className="rounded-[12px]">
         <div className="py-4 px-6 overflow-hidden" onClick={handleDialogClick}>
           {children}
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -5,3 +5,19 @@ export function getDecoderLocLink(longitude: number, latitude: number) {
     4
   )}&output=html&coord_type=wgs84&src=webapp.baidu.openAPIdemo`;
 }
+
+/**
+ * 锁定 body 滚动，避免弹窗问题
+ */
+export function lockBodyScroll() {
+  document.body.style.top = - document.documentElement.scrollTop + 'px';
+  document.body.style.position = 'fixed';
+}
+
+/**
+ * 解锁 body 滚动
+ */
+export function unlockBodyScroll() {
+  document.body.style.position = '';
+  document.body.style.top = '';
+}
